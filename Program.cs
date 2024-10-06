9bf5ef8ae42993c97fce623e90fe9d3bbea8c227
illyakich <speedfeed123@gmail.com>
2024-10-06 22:21:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿int Countdown(int number)
 {
     Console.WriteLine(number);
-    if (number != 0) // only execute if the number doesnot equal to 0
+    if (number != 0) // only execute if the number doesnot equal to 0.
         return number - Countdown(number - 1); // pass the value to the parameter, then execute this method again but subtract 1 from the passed value.
     return number; // the code doesnot work without this idk.
 }
