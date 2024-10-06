662e99d90128cb3e9b4369a4074054e9c3250de7
illyakich <speedfeed123@gmail.com>
2024-10-06 22:24:09 +0300

    now less confusing!

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     Console.WriteLine(number);
     if (number != 0) // only execute if the number doesnot equal to 0.
-        return number - Countdown(number - 1); // pass the value to the parameter, then execute this method again but subtract 1 from the passed value.
+    Countdown(number - 1); // pass the value to the parameter, then execute this method again but subtract 1 from the passed value.
     return number; // the code doesnot work without this idk.
 }
 Countdown(10);
